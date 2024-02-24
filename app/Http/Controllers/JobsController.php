22e80caa56462ec7d59d30cbb6c9ae42d68be7bd
Rajesh Mondal <rajesh01749@gmail.com>
2024-02-24 14:20:32 +0600

    work on job details page

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -62,4 +62,18 @@ class JobsController extends Controller {
             'jobTypeArray' => $jobTypeArray,
         ] );
     }
+
+    public function detail( $id ) {
+
+        $job = Job::where( [
+            'id'     => $id,
+            'status' => 1,
+        ] )->with( ['jobType', 'category'] )->first();
+
+        if ( $job == null ) {
+            abort( 404 );
+        }
+
+        return view( 'front.jobDetail', ['job' => $job] );
+    }
 }
