64db4bc92193010b47c5b85c1218ab658050732e
Rajesh Mondal <rajesh01749@gmail.com>
2024-02-29 19:59:11 +0600

    delete job application (admin panel)

--- app/Http/Controllers/Admin/JobApplicationController.php
+++ app/Http/Controllers/Admin/JobApplicationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\JobApplication;
+use Illuminate\Http\Request;
 
 class JobApplicationController extends Controller {
     public function index() {
@@ -12,6 +13,24 @@ class JobApplicationController extends Controller {
             ->paginate( 10 );
         return view( 'admin.job-applications.list', [
             'applications' => $applications,
+        ] );
+    }
+
+    public function destroy( Request $request ) {
+        $id = $request->id;
+        $jobApplication = JobApplication::find( $id );
+
+        if ( $jobApplication == null ) {
+            session()->flash( 'error', 'Either job application deleted or not found.' );
+            return response()->json( [
+                'status' => false,
+            ] );
+        }
+
+        $jobApplication->delete();
+        session()->flash( 'success', 'Job application deleted successfully.' );
+        return response()->json( [
+            'status' => true,
         ] );
     }
 }
