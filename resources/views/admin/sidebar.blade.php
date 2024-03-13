4e4c0f359a51974bd9288b90f179d51a7d83072d
Rajesh Mondal <rajesh01749@gmail.com>
2024-03-13 23:50:23 +0600

    working on blog functionality

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -25,6 +25,9 @@
                 <a href="{{ route('admin.jobApplications') }}">Job Applications</a>
             </li>
             <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                <a href="{{ route('admin.blog.index') }}">Blogs</a>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
                 <a href="{{ route('account.logout') }}">Logout</a>
             </li>                                                        
         </ul>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\BlogController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\JobApplicationController;
 use App\Http\Controllers\Admin\JobController;
